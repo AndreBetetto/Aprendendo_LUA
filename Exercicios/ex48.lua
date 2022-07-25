2ad624c257c5415732d3e726b24d85ab87fa565f
AndreBetetto <andre.betetto@unesp.br>
2022-07-25 12:05:09 -0300

    update

---
+++ Exercicios/ex48.lua
@@ -0,0 +1,19 @@
+local x = io.read()
+x = tonumber(x)
+
+if x > 100 then
+    print("O numero e maior que 100.")
+elseif x < 100 then
+    print("O numero e menor que 100.")
+elseif x == 100 then
+    print("O numero e igual a 100.")   
+else
+    print("A entrada digitada nao e um numero.")     
+end
+
+
+
+
+
+
+
